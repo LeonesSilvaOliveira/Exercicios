5724bf3c59541b79d3ff040d95526de142bb4eb6
Leones Silva de Oliveira <50273318+LeonesSilvaOliveira@users.noreply.github.com>
2024-12-05 15:33:57 -0300

    update
    test

---
+++ Exercício 4/app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Olá, esta é a minha primeira API com Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
